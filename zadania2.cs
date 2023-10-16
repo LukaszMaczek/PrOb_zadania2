0309fb6f5c06919e1eb155d7ebf82802b818d8d0
Łukasz Maczek <lukaszmaczek@student.agh.edu.pl>
2023-10-16 21:22:49 +0200

    Add project files.

---
+++ zadania2.cs
@@ -0,0 +1,10 @@
+﻿namespace zadania_2
+{
+    internal class zadania2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
