c943166384bca9f577ba2cd94a6ce89f713f2127
Łukasz Maczek <lukaszmaczek@student.agh.edu.pl>
2023-10-16 21:38:07 +0200

    pierwsze

--- zadania2.cs
+++ zadania2.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            OcenyPozytywne();
+        }
+        static void OcenyPozytywne()
+        {
+            int suma = 0;
+            int ocena;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Podaj ocenę: ");
+                ocena = Convert.ToInt32(Console.ReadLine());
+                suma += ocena;
+            }
+            double srednia = suma / (double)5;
+            Console.WriteLine("Śrenia z ocen wynosi: {0:N}", srednia);
+
         }
     }
 }
